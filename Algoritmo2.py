9d0c276ecd1634448e094edcb7a7e31e568dac37
Alejoso <atirador1@eafit.edu.co>
2025-03-18 22:42:25 -0500

    Working Algorithm 2

--- Algoritmo2.py
+++ Algoritmo2.py
@@ -18,7 +18,35 @@ for line in lines[4:]:
 class PDA:
     def __init__(self):
         self.starting_pile_symbol = "S" # We are only going to use the acceptance judgement on the empty stack, so no need to stablish an acceptance state
-    
+        self.starting_state = "q0"
+
+    def transitions(self , stack , current_state , char):
+        
+        if(len(stack) != 0):
+            
+            if (current_state == "q0" and char == "" and stack[-1] == "S"):
+                stack.pop()
+                return 1
+            elif (current_state == "q0" and char == "a" and stack[-1] == "S"):
+                stack.pop()
+                stack.append("a")
+                return 2
+            elif (current_state == "q0" and char == "a" and stack[-1] == "a"):
+                stack.append("a")
+                return 3
+            elif (current_state == "q0" and char == "b" and stack[-1] == "a"):
+                stack.pop()
+                current_state = "q1"
+                return 4
+            elif (current_state == "q1" and char == "b" and stack[-1] == "a"):
+                stack.pop()
+                return 5
+            else:
+                return -1
+        else:
+            return -1
+ 
+        
     def is_valid(self, string):
         stack = []
         stack.append(self.starting_pile_symbol)
@@ -26,20 +54,23 @@ class PDA:
         if len(string) == 0:
             return True
         
-        for char in string:
-            if char == "a":
-                stack.append(char)
-            elif char == "b":
-                if len(stack) == 0:
-                    return False
-                if stack[-1] == "a":
-                    stack.pop()
-            print(stack)
-        
+        transitions_done = []
 
-        if len(stack) == 1 and stack[0] == self.starting_pile_symbol:
-            stack.pop()
-            return True
+        for char in string:
+            transitions_done.append(self.transitions(stack, self.starting_state, char))       
+        
+        print(transitions_done)
+
+        for transition in transitions_done:
+            if (transition == -1):
+                return False
+            
+        if (len(stack) != 0):
+            return False
+        
+        return True
+
+
         
        
 pda = PDA()
