5d9a865c3da9ed17bb128f89b52623961053b911
Alejoso <atirador1@eafit.edu.co>
2025-03-18 22:00:52 -0500

    Adding some core functionalities

--- Algoritmo2.py
+++ Algoritmo2.py
@@ -14,5 +14,41 @@ for line in lines[4:]:
     if len(parts) > 1:
         string_list.append(parts[1])  #Add strings where the shape is correct
 
-print(string_list)
+
+class PDA:
+    def __init__(self):
+        self.starting_pile_symbol = "S" # We are only going to use the acceptance judgement on the empty stack, so no need to stablish an acceptance state
+    
+    def is_valid(self, string):
+        stack = []
+        stack.append(self.starting_pile_symbol)
+
+        if len(string) == 0:
+            return True
+        
+        for char in string:
+            if char == "a":
+                stack.append(char)
+            elif char == "b":
+                if len(stack) == 0:
+                    return False
+                if stack[-1] == "a":
+                    stack.pop()
+            print(stack)
+        
+
+        if len(stack) == 1 and stack[0] == self.starting_pile_symbol:
+            stack.pop()
+            return True
+        
+       
+pda = PDA()
+
+for string in string_list:
+    accepted = pda.is_valid(string)
+    if accepted:
+        print(f"The string '{string}' is accepted by the grammar")
+    else:
+        print(f"The string '{string}' is not accepted by the grammar")
+
 
